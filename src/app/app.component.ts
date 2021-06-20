d009aa26d12e1a7459eee76f2b4cc508355bfb11
Rajdave123 <54428814+Rajdave123@users.noreply.github.com>
2021-06-20 16:52:35 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing';
+
+  text = '';
+
+  sentences = [
+    'ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate',
+  ];
+
+  sentence = this.sentences[Math.floor(Math.random() * this.sentences.length)];
+
+  changeText(e: any) {
+    this.text = e.target.value;
+  }
+
+  compare(randomeLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomeLetter === enteredLetter ? 'success' : 'danger';
+  }
 }
