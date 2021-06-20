bb75bf3796c915e274ad1c7fcd43533c324f2048
Rajdave123 <54428814+Rajdave123@users.noreply.github.com>
2021-06-20 18:07:22 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,22 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
-
+  sentence = lorem.sentence();
   text = '';
-
-  sentences = [
-    'ten ate neite ate nee enet ite ate inet ent eate',
-    'Too ato too nOt enot one totA not anot tOO aNot',
-    'oat itain oat tain nate eate tea anne inant nean',
-    'itant eate anot eat nato inate eat anot tain eat',
-    'nee ene ate ite tent tiet ent ine ene ete ene ate',
-  ];
-
-  sentence = this.sentences[Math.floor(Math.random() * this.sentences.length)];
 
   changeText(e: any) {
     this.text = e.target.value;
+  }
+
+  next() {
+    this.sentence = lorem.sentence();
+    // console.log(1212);
+    this.text = "";
   }
 
   compare(randomeLetter: string, enteredLetter: string) {
